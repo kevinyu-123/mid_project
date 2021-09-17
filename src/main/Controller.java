4f7f04ba13a5e39368b980d4ef4b57e7cf09ed9b
HanGilyong <shkg13@naver.com>
2021-09-17 16:29:15 +0900

    first commit

---
+++ src/main/Controller.java
@@ -0,0 +1,12 @@
+package main;
+
+import javafx.scene.Parent;
+
+public class Controller {
+	Parent root;
+	public void setRoot(Parent root) {this.root=root;}
+	public void fxAdminMenu(){}
+	public void fxLogin(){}
+	public void fxMemInsert(){}
+	public void fxLookingPw(){}
+}
---
+++ src/main/MainClass.java
@@ -0,0 +1,25 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainClass extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+		Parent root = loader.load();
+		Controller ctl = loader.getController();
+		ctl.setRoot(root);
+		arg0.setScene(new Scene(root));
+		arg0.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
