60212435134dd952f85f492276db2115abdb3319
yhj <tntj123@naver.com>
2021-09-17 19:36:30 +0900

    오류 수정

--- src/db/DBService.java
+++ src/db/DBService.java
@@ -4,7 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class dbService {
+public class DBService {
 	String sql;
 	int result;
 	PreparedStatement ps;
